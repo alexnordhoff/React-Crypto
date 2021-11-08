3b4a64716214cfad27a4d5f669b9e6c1b89f6af8
Alex Nordhoff <73557166+alexnordhoff@users.noreply.github.com>
2021-11-08 16:08:38 -0600

    added sha-256 hashing for input text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+const { createHash } = require('crypto');
 
 function App() {
+  const str = "Text to Encode:    ";
+  const [enteredText, setEnteredText] = useState("");
+  const [hash_256, set256Hash] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  function hash(string) {
+    set256Hash(createHash('sha256').update(string).digest('hex'));
+    
+
+
+    setSubmitted(true);
+    return
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Enter some text below to begin the encoding: 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <label>
+            {str} 
+            <input type="text" name="input" onChange={(e) => setEnteredText(e.target.value)}/>
+          </label>
+          <input type="submit" value="Submit" onClick={()=>hash(enteredText)}/>
+        </div>
+        <div style={{textAlign: 'center', alignSelf: 'center'}}>
+        {submitted && 
+          <p style={{fontSize:12}}>
+            SHA-256 Hash Value: {hash_256}
+          </p>
+        }
+      </div>
       </header>
+      
     </div>
   );
 }
