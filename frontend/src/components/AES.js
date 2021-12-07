404ebedbb64eccaa5656d81c20ec85a5b3496e3e
Ian Reitmaier <ianreitmaier@Ians-MacBook-Pro.local>
2021-12-07 12:43:36 -0600

    finishing AES

--- frontend/src/components/AES.js
+++ frontend/src/components/AES.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-
+import { Card } from 'react-bootstrap';
 
 var CryptoJS = require("crypto-js");
 
@@ -16,10 +16,10 @@ function AES() {
         setCipher(CryptoJS.AES.encrypt(JSON.stringify(string1), string2).toString());
         return
     }
-    async function decryptAES(string1, string2)
+    function decryptAES(string1, string2)
     {
         setByte(CryptoJS.AES.decrypt(string1, string2).toString(CryptoJS.enc.Utf8));
-        await setDecryptData(byte);
+        setDecryptData(byte);
         return
     }
 
@@ -28,14 +28,33 @@ function AES() {
             <div class = "row" style={{alignContent: 'center'}}>
                 <h1>AES</h1>
             </div>
-
             <div class="row" style={{alignContent: 'center'}}>
                 <input class="form-control" placeholder="Enter a Key:" type="text" name="input" onChange={(e) => setKey(e.target.value)}/>
                 <input class="form-control" placeholder="Enter a PlainText:" type="text" name="input" onChange={(e) => setText(e.target.value)}/>
                 <button class="btn btn-primary" style={{alignContent: 'center'}} type="submit" onClick={()=>encryptAES(text, key)}> Submit </button>
-                <p>{cipher}</p>
+                {/* <p>{cipher}</p> */}
+                <div class="row" style={{marginTop: 10, marginBottom: 10, alignContent: "center"}}>
+                  <Card>
+                    <Card.Header as="h5">Cipher Text</Card.Header>
+                    <Card.Body>
+                      <Card.Text>
+                        {cipher}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
                 <button class="btn btn-primary" style={{alignContent: 'center'}} type="submit" onClick={()=>decryptAES(cipher, key)}> Decrypt </button>
-                <p>{decryptData}</p>
+                {/* <p>{decryptData}</p> */}
+                <div class="row" style={{marginTop: 10, marginBottom: 10, alignContent: "center"}}>
+                  <Card>
+                    <Card.Header as="h5">Decrypted Text</Card.Header>
+                    <Card.Body>
+                      <Card.Text>
+                        {decryptData}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </div>
             </div>
         </div>
     )
